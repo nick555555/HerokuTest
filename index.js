e1a998e4d39c054455b69dc9fe0dfb9f2adc6126
Nick <57273851+nick555555@users.noreply.github.com>
2020-01-08 10:48:34 -0800

    more stuff added

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+const PORT = procss.env.PORT || 3000;
+
+app.get('/', (req,res)=>{
+    res.send('Welcome to my API');
+})
+
+app.listen(PORT,function(){
+    console.log('listening on port ' + PORT);
+})
